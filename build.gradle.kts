39009f42bed6d6c835d8fb90f8afc482d7627b3c
microvencn <95117247+microvencn@users.noreply.github.com>
2024-04-08 17:15:24 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("org.gradle.java")
     id("org.gradle.maven-publish")
     kotlin("jvm") version "1.8.0" apply false
-    id("io.izzel.taboolib") version "2.0.11" apply false
+    id("io.izzel.taboolib") version "1.56" apply false
 }
 
 description = "Modern & Advanced Menu-Plugin for Minecraft Servers"
